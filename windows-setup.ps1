e8c0862e9a88b2a390497c73646d91ff5a8041f4
ditsuke <ditsuke@protonmail.com>
2022-08-23 00:56:25 +0530

    add windows setup script

---
+++ windows-setup.ps1
@@ -0,0 +1,13 @@
+# Get stuff from winget
+
+$apps = @("9NHL4NSC67WM", "Loom.Loom", "9N97ZCKPD60Q", "5319275A.WhatsAppDesktop_cv1g1gvanyjgm", "ShareX.ShareX", "BraveSoftware.BraveBrowser", "Discord.Discord", "Docker.DockerDesktop", "SlackTechnologies.Slack", "PenguinLabs.Cacher", "Microsoft.Teams", "tailscale.tailscale", "SpotifyAB.SpotifyMusic_zpdnekdrzrea0", "OBSProject.OBSStudio", "Mozilla.Firefox", "Microsoft.PowerToys", "Microsoft.VisualStudioCode", "Zoom.Zoom", "QL-Win.QuickLook", "Jetbrains.Toolbox")
+
+foreach ($app in $apps) { winget install --id=$app -e }
+
+# Install the scoop package manager (I prefer it for command line utilities, portable software)
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+irm get.scoop.sh | iex
+
+# Install things
+scoop import $PsScriptRoot/.windows/scoopfile.json
+
