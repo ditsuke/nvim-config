5d671e725bc74adb395bf59b7812dba492f3fb1e
ditsuke <ditsuke@protonmail.com>
2022-08-23 02:52:10 +0530

    add authy

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -1,6 +1,6 @@
 # Get stuff from winget
 
-$apps = @("9NHL4NSC67WM", "Loom.Loom", "9N97ZCKPD60Q", "5319275A.WhatsAppDesktop_cv1g1gvanyjgm", "ShareX.ShareX", "BraveSoftware.BraveBrowser", "Discord.Discord", "Docker.DockerDesktop", "SlackTechnologies.Slack", "PenguinLabs.Cacher", "Microsoft.Teams", "tailscale.tailscale", "SpotifyAB.SpotifyMusic_zpdnekdrzrea0", "OBSProject.OBSStudio", "Mozilla.Firefox", "Microsoft.PowerToys", "Microsoft.VisualStudioCode", "Zoom.Zoom", "QL-Win.QuickLook", "Jetbrains.Toolbox")
+$apps = @("9NHL4NSC67WM", "Loom.Loom", "9N97ZCKPD60Q", "5319275A.WhatsAppDesktop_cv1g1gvanyjgm", "ShareX.ShareX", "BraveSoftware.BraveBrowser", "Discord.Discord", "Docker.DockerDesktop", "SlackTechnologies.Slack", "PenguinLabs.Cacher", "Microsoft.Teams", "tailscale.tailscale", "SpotifyAB.SpotifyMusic_zpdnekdrzrea0", "OBSProject.OBSStudio", "Mozilla.Firefox", "Microsoft.PowerToys", "Microsoft.VisualStudioCode", "Zoom.Zoom", "QL-Win.QuickLook", "Jetbrains.Toolbox", "voidtools.Everything", "Twilio.Authy")
 
 foreach ($app in $apps) { winget install --id=$app -e }
 
