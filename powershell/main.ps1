2172745265243974fe431f5c54d2670db94abcc1
ditsuke <ditsuke@protonmail.com>
2022-08-28 16:12:28 +0530

    improve powershell keybinds

--- powershell/main.ps1
+++ powershell/main.ps1
@@ -1,17 +1,3 @@
-# Cursor moves to the end of line
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-
-# Vi mode, no annoying bell
-Set-PSReadLineOption -EditMode Vi -BellStyle None
-
-# Shows navigable menu of all options when hitting Tab
-# Look into Powertab for a better option
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-# Autocompletion for arrow keys
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-
 # Set dotfiles path (PS module)
 $DotFilesPath = "$HOME/dotfiles"
 
@@ -23,4 +9,3 @@ $DotFilesPath = "$HOME/dotfiles"
 . "$PSScriptRoot/internal/autocompletions.ps1"
 . "$PSScriptRoot/internal/loads.ps1"
 . "$PSScriptRoot/internal/key-config.ps1"
-
