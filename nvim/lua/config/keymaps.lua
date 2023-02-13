40ee0edba834a6b7c3f2e743c88a3f7f3d567d2a
ditsuke <ditsuke@protonmail.com>
2023-02-13 15:35:21 +0530

    update: neovim config
    Improves mappings for command-mode completions

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,13 +16,26 @@ vim.keymap.set("n", "N", "Nzz")
 -- ===================
 
 -- Navigate tab completion w/ <C-j> and <C-k>
--- TODO: noremap = true
-vim.keymap.set("c", "<C-k>", function()
-  return vim.fn.pumvisible() == 1 and "<C-p>" or "<Up>"
-end, { expr = true })
-vim.keymap.set("c", "<C-j>", function()
-  return vim.fn.pumvisible() == 1 and "<C-n>" or "<Down>"
-end, { expr = true })
+
+--- Map `key` to `mapped_to` if the nvim-native popup
+--- menu is visible.
+---
+--- @param mode string  Mode shortstring
+--- @param lhs  string  LHS
+--- @param rhs  string  RHS
+---
+--- @see https://vim.fandom.com/wiki/Improve_completion_popup_menu
+local function map_if_pumvisible(mode, lhs, rhs)
+  vim.keymap.set(mode, lhs, function()
+    return vim.fn.pumvisible() == 1 and rhs or lhs
+  end, { expr = true, noremap = true })
+end
+
+-- Better command-completion mappings
+map_if_pumvisible("c", "<C-k>", "<C-p>")
+map_if_pumvisible("c", "<C-j>", "<C-n>")
+map_if_pumvisible("c", "<Esc>", "<C-e>")
+map_if_pumvisible("c", "<CR>", "<C-y>")
 
 -- ===================
 -- Behavior
