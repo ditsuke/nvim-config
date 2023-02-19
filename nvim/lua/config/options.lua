8cf06dc29910f0a37779a9ae2470195b6ea2be13
ditsuke <ditsuke@protonmail.com>
2023-02-19 18:20:42 +0530

    nvim: cwd in window title
    With the caveat that it doesn't update on cwd changes
    
    atm.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,3 +6,9 @@ local opt = vim.opt
 
 opt.clipboard = "" -- **Do not** sync with the system clipboard
 opt.mouse = "v" -- Disable mouse support by default
+
+-- Make window title indicative of our cwd
+-- TODO:auto-update on cwd change
+local path = vim.split(vim.fn.getcwd(), "/", {})
+vim.opt.titlestring = [[%{v:progname}: ]] .. vim.fn.join({ path[#path - 1], path[#path] }, "/")
+vim.opt.title = true
