8190c1f94327bb7d481986daa00f8ab9b8763e8a
ditsuke <ditsuke@protonmail.com>
2023-03-12 03:43:33 +0530

    nvim: autoload session

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -18,3 +18,27 @@ vim.api.nvim_create_autocmd({ "DirChanged", "VimEnter" }, {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = { "LazyVimStarted" },
+  group = vim.api.nvim_create_augroup("LoadSession", { clear = true }),
+  callback = function()
+    local does_persist, persistence = pcall(require, "persistence")
+    if does_persist then
+      -- vim.loop.timer_start(timer, 1, 0, function()
+      --   vim.schedule(function()
+      --     persistence.load()
+      --     print("done loadin since 69")
+      --   end)
+      -- end)
+
+      -- Appanrently I don't even have to delay it, just send it to the event loop
+      -- If I load it right here, the session loads fine but buffers don't have treesitter etc
+      -- To me this indicates that Lazyvim's `started` event is fired while plugins are
+      -- still in a queue sent to the uv event loop, might be unwanted behavioe on lazy.nvim's end
+      vim.schedule(persistence.load)
+      print("done sleepin' since 69")
+    end
+    vim.api.nvim_del_augroup_by_name("LoadSession")
+  end,
+})
