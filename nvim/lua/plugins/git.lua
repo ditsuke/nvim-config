c65c1939e68edc13865f663d4b9069542d729d7b
ditsuke <ditsuke@protonmail.com>
2023-03-17 22:15:14 +0530

    nvim: inline git-blame

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true, -- Inline blame for current line w/ virt-text
+    },
+  },
+  {
     "ThePrimeagen/git-worktree.nvim",
     event = "VeryLazy",
     -- ! For some reason declaring this dependency overrides telescope.nvim setup in lazyvim core
