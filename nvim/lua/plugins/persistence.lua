e38f6d06c8a869247adfbb1a4b82a102ae368ef4
ditsuke <ditsuke@protonmail.com>
2023-03-18 20:23:15 +0530

    nvim: fix session autoload conflicting with lazy.nvim

--- nvim/lua/plugins/persistence.lua
+++ nvim/lua/plugins/persistence.lua
@@ -5,11 +5,13 @@ return {
     "folke/persistence.nvim",
     lazy = false,
     event = "VimEnter",
-    init = function()
-      local does_persist, persistence = pcall(require, "persistence")
-      if not does_persist then
-        return
-      end
+    opts = {
+      -- Include globals to persist special things, such as pinned buffers
+      options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" },
+    },
+    config = function(_, opts)
+      local persistence = require("persistence")
+      persistence.setup(opts)
 
       vim.api.nvim_create_user_command(CMD_NO_SESSION, function(_) end, {}) -- Is the requirement misdocumented in neodev?
       vim.api.nvim_create_autocmd("User", {
@@ -20,27 +22,24 @@ return {
             require("persistence").stop()
             return
           end
-          -- vim.loop.timer_start(timer, 1, 0, function()
-          --   vim.schedule(function()
-          --     persistence.load()
-          --     print("done loadin since 69")
-          --   end)
-          -- end)
-
-          -- Appanrently I don't even have to delay it, just send it to the event loop
+          -- Appanrently I don't even have to delay it with `timer_start`, just send it to main vim loop.
           -- If I load it right here, the session loads fine but buffers don't have treesitter etc
           -- To me this indicates that Lazyvim's `started` event is fired while plugins are
-          -- still in a queue sent to the uv event loop, might be unwanted behavioe on lazy.nvim's end
-          vim.schedule(persistence.load)
-          print("done sleepin' since 69")
+          -- still in a queue sent to the vim event loop, might be unwanted behavioe on lazy.nvim's end
+          if vim.o.filetype ~= "lazy" then
+            vim.schedule(persistence.load)
+            return
+          end
+
+          vim.cmd.close()
+          vim.schedule(function()
+            persistence.load()
+            require("lazy").show()
+          end)
           vim.api.nvim_del_augroup_by_name("LoadSession")
           vim.api.nvim_del_user_command(CMD_NO_SESSION)
         end,
       })
     end,
-    opts = {
-      -- Include globals to persist special things, such as pinned buffers
-      options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" },
-    },
   },
 }
