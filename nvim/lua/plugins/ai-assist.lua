04d7ab61b2361f9a2150db8fdca1d5b4ec0dbc58
ditsuke <ditsuke@protonmail.com>
2023-03-19 21:18:21 +0530

    nvim: add chatgpt.nvim

--- nvim/lua/plugins/ai-assist.lua
+++ nvim/lua/plugins/ai-assist.lua
@@ -9,4 +9,17 @@ return {
   {
     "zbirenbaum/copilot-cmp",
   },
+  {
+    "jackMort/ChatGPT.nvim",
+    config = true,
+    cond = function()
+      local api_key = os.getenv("OPENAI_API_KEY")
+      return api_key and api_key ~= "" and true or false
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
 }
