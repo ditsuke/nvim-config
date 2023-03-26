ed67573c59dc0e225e37f8beff7f39fcef77cc76
ditsuke <ditsuke@protonmail.com>
2023-03-26 14:18:12 +0530

    nvim: Sync terminal background

--- nvim/lua/plugins/appearance-colorschemes.lua
+++ nvim/lua/plugins/appearance-colorschemes.lua
@@ -112,6 +112,9 @@ return {
       event = "VeryLazy",
     },
   },
+  -- Effortlessly sync the terminal background with Neovim.
+  -- As a side effect, get effortless transparency across color schemes!
+  { "typicode/bg.nvim", lazy = false, cond = function() return os.getenv("NVIM_COLORSYNC") == nil end },
   -- Configure LazyVim to load oxocarbon
   {
     "LazyVim/LazyVim",
