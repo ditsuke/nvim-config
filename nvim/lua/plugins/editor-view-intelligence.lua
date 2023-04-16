f2ecf9b009b006d8506ec61a0691abcc86ddcbb0
ditsuke <ditsuke@protonmail.com>
2023-04-16 14:25:26 +0530

    nvim: update treesitter module configurations
    - Disable most modules on large files (>2000 LOC)
    - Re-enable markid
    - Enable scope highlight with treesitter-refactor
    
    And probably some more things.

--- nvim/lua/plugins/editor-view-intelligence.lua
+++ nvim/lua/plugins/editor-view-intelligence.lua
@@ -1,12 +1,21 @@
+local disable_on_large_buffers = function(_, bufnr)
+  local TS_MAX_LINE = 2000
+  return vim.api.nvim_buf_line_count(bufnr) > TS_MAX_LINE
+end
+
 return {
-  -- Show code context (function, etc) on top while navigating ala context.vim
   {
     "nvim-treesitter/nvim-treesitter",
+    enabled = true,
+    init = nil,
     dependencies = {
+      { "nvim-treesitter-textobjects", enabled = false, init = nil }, -- Disable textobjects, I'm not using it anyways
       "nvim-treesitter/playground",
+      -- highlight same-name identifiers with the same color
       "David-Kunz/markid",
       "HiPhish/nvim-ts-rainbow2",
       "nvim-treesitter/nvim-treesitter-refactor",
+      -- Show code context (function, etc) on top while navigating ala context.vim
       "nvim-treesitter/nvim-treesitter-context",
       "theHamsta/nvim-dap-virtual-text",
       "windwp/nvim-ts-autotag",
@@ -14,24 +23,35 @@ return {
     },
     opts = {
       auto_install = true,
+      highlight = {
+        disable = disable_on_large_buffers,
+      },
       markid = {
-        enable = false, -- Perf impact high on big (5000LOC :3) files -- I'll maybe have it configurable with neoconf later
+        enable = true,
+        disable = disable_on_large_buffers,
       },
       rainbow = {
         enable = true,
         query = "rainbow-parens",
-        max_file_lines = 3000,
+        max_file_lines = 2000,
       },
       refactor = {
         enable = true,
-        clear_on_cursor_move = false,
-        highlight_definitions = { enable = true },
+        disable = disable_on_large_buffers,
+        highlight_definitions = {
+          enable = true,
+          clear_on_cursor_move = false,
+        },
+        highlight_current_scope = { enable = true },
+        -- TODO: consider using goto-definition etc
       },
       autotag = {
         enable = true,
+        disable = disable_on_large_buffers,
       },
       matchup = {
         enable = true,
+        disable = disable_on_large_buffers,
       },
     },
     keys = {
@@ -49,6 +69,7 @@ return {
   },
   {
     "Djancyp/better-comments.nvim",
+    enabled = false,
     name = "better-comment.nvim",
     config = function(_, opts) require("better-comment").Setup(opts) end,
   },
