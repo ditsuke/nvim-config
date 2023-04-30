23747ab8ceea4d8f8151fec912ee703bc1254129
ditsuke <ditsuke@protonmail.com>
2023-05-01 03:20:41 +0530

    fix(lang): rust-analyzer setup

--- lua/ditsuke/extras/lang/rust.lua
+++ lua/ditsuke/extras/lang/rust.lua
@@ -30,7 +30,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "simrat39/rust-tools.nvim",
+      { "simrat39/rust-tools.nvim" },
     },
     opts = {
       servers = {
@@ -38,84 +38,85 @@ return {
         rust_analyzer = {},
         taplo = {},
       },
-    },
-    setup = {
-      rust_analyzer = function(_, opts)
-        require("lazyvim.util").on_attach(function(client, buffer)
+      setup = {
+        rust_analyzer = function(_, opts)
+          require("lazyvim.util").on_attach(function(client, buffer)
 						-- stylua: ignore
 						if client.name == "rust_analyzer" then
+              require("ditsuke.utils").logger("we found rust_analyzer workin")
 							vim.keymap.set("n", "K", "<cmd>RustHoverActions<cr>", { buffer = buffer, desc = "Hover Actions (Rust)" })
 							vim.keymap.set("n", "<leader>cR", "<cmd>RustCodeAction<cr>", { buffer = buffer, desc = "Code Action (Rust)" })
 							vim.keymap.set("n", "<leader>dr", "<cmd>RustDebuggables<cr>", { buffer = buffer, desc = "Run Debuggables (Rust)" })
 						end
-        end)
-        local mason_registry = require("mason-registry")
-        -- rust tools configuration for debugging support
-        local codelldb = mason_registry.get_package("codelldb")
-        local extension_path = codelldb:get_install_path() .. "/extension/"
-        local codelldb_path = extension_path .. "adapter/codelldb"
-        local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
-          or extension_path .. "lldb/lib/liblldb.so"
-        local rust_tools_opts = vim.tbl_deep_extend("force", opts, {
-          dap = {
-            adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-          },
-          tools = {
-            on_initialized = function()
-              vim.cmd([[
+          end)
+          local mason_registry = require("mason-registry")
+          -- rust tools configuration for debugging support
+          local codelldb = mason_registry.get_package("codelldb")
+          local extension_path = codelldb:get_install_path() .. "/extension/"
+          local codelldb_path = extension_path .. "adapter/codelldb"
+          local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
+            or extension_path .. "lldb/lib/liblldb.so"
+          local rust_tools_opts = vim.tbl_deep_extend("force", opts, {
+            dap = {
+              adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+            },
+            tools = {
+              on_initialized = function()
+                vim.cmd([[
 									augroup RustLSP
 										autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
 										autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
 										autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
 									augroup END
 								]])
-            end,
-          },
-          server = {
-            settings = {
-              ["rust-analyzer"] = {
-                cargo = {
-                  allFeatures = true,
-                  loadOutDirsFromCheck = true,
-                  runBuildScripts = true,
-                },
-                -- Add clippy lints for Rust.
-                checkOnSave = {
-                  allFeatures = true,
-                  command = "clippy",
-                  extraArgs = { "--no-deps" },
-                },
-                procMacro = {
-                  enable = true,
-                  ignored = {
-                    ["async-trait"] = { "async_trait" },
-                    ["napi-derive"] = { "napi" },
-                    ["async-recursion"] = { "async_recursion" },
+              end,
+            },
+            server = {
+              settings = {
+                ["rust-analyzer"] = {
+                  cargo = {
+                    allFeatures = true,
+                    loadOutDirsFromCheck = true,
+                    runBuildScripts = true,
+                  },
+                  -- Add clippy lints for Rust.
+                  checkOnSave = {
+                    allFeatures = true,
+                    command = "clippy",
+                    extraArgs = { "--no-deps" },
+                  },
+                  procMacro = {
+                    enable = true,
+                    ignored = {
+                      ["async-trait"] = { "async_trait" },
+                      ["napi-derive"] = { "napi" },
+                      ["async-recursion"] = { "async_recursion" },
+                    },
                   },
                 },
               },
             },
-          },
-        })
-        require("rust-tools").setup(rust_tools_opts)
-        return true
-      end,
-      taplo = function(_, _)
-        local function show_documentation()
-          if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-            require("crates").show_popup()
-          else
-            vim.lsp.buf.hover()
+          })
+          require("rust-tools").setup(rust_tools_opts)
+          return true
+        end,
+        taplo = function(_, _)
+          local function show_documentation()
+            if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+              require("crates").show_popup()
+            else
+              vim.lsp.buf.hover()
+            end
           end
-        end
-        require("lazyvim.util").on_attach(function(client, buffer)
+          require("lazyvim.util").on_attach(function(client, buffer)
 						-- stylua: ignore
 						if client.name == "taplo" then
 							vim.keymap.set("n", "K", show_documentation, { buffer = buffer, desc = "Show Crate Documentation" })
 						end
-        end)
-        return false -- make sure the base implementation calls taplo.setup
-      end,
+          end)
+          return false -- make sure the base implementation calls taplo.setup
+        end,
+      },
     },
   },
 }
