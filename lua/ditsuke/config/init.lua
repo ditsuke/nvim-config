4be24462e11f4466cd6d511a87755aa10b635a39
ditsuke <ditsuke@protonmail.com>
2023-04-30 18:01:07 +0530

    fix: load options fr

--- lua/ditsuke/config/init.lua
+++ lua/ditsuke/config/init.lua
@@ -2,6 +2,8 @@ local M = {}
 
 function M.init()
   if vim.fn.argc(-1) == 0 then
+    require("ditsuke.config.options")
+
     -- autocmds and keymaps can wait to load
     vim.api.nvim_create_autocmd("User", {
       group = vim.api.nvim_create_augroup("ditsukeCfg", { clear = true }),
