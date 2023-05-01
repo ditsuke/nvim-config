e67d89a97e7d16fad372e9c7ec4633ef6328be69
ditsuke <ditsuke@protonmail.com>
2023-05-02 01:31:09 +0530

    chore(prose): Disable vim's native spellcheck
    Because it sucks

--- lua/ditsuke/extras/prose.lua
+++ lua/ditsuke/extras/prose.lua
@@ -3,6 +3,10 @@ local Util = require("ditsuke.utils")
 return {
   {
     "jose-elias-alvarez/null-ls.nvim",
+    init = function()
+      -- Disable vim's own spell checker (it sucks)
+      vim.opt.spell = false
+    end,
     opts = function(_, opts)
       local null_ls = require("null-ls")
       Util.list_insert_unique(opts.sources, {
