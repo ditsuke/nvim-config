984ae8f6eb95f378822e3a06b3af18ba99ea1cc8
ditsuke <ditsuke@protonmail.com>
2023-05-02 02:15:09 +0530

    chore(prose): Decrease diagnostic severity

--- lua/ditsuke/extras/prose.lua
+++ lua/ditsuke/extras/prose.lua
@@ -11,7 +11,9 @@ return {
       local null_ls = require("null-ls")
       Util.list_insert_unique(opts.sources, {
         null_ls.builtins.code_actions.proselint,
-        null_ls.builtins.diagnostics.cspell,
+        null_ls.builtins.diagnostics.cspell.with({
+          diagnostics_postprocess = function(diagnostic) diagnostic.severity = vim.diagnostic.severity["HINT"] end,
+        }),
         null_ls.builtins.code_actions.cspell,
       })
       return opts
