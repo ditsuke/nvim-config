fbbf3593086509ebfa6576a3606ec06737b21e53
ditsuke <ditsuke@protonmail.com>
2023-05-02 03:50:05 +0530

    feat(editor): Better escape with jk, jj

--- lua/ditsuke/plugins/editor-behavior.lua
+++ lua/ditsuke/plugins/editor-behavior.lua
@@ -92,4 +92,13 @@ return {
     "lukas-reineke/virt-column.nvim",
     config = true,
   },
+  {
+    "max397574/better-escape.nvim",
+    opts = {
+      mapping = { "jk", "jj" },
+      timeout = 200,
+      clear_empty_lines = true,
+      keys = "<Esc>",
+    },
+  },
 }
