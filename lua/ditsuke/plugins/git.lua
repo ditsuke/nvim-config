96efc2e0c38b6a26f3b215aa0384aeefdda431c3
ditsuke <ditsuke@protonmail.com>
2023-05-11 20:15:18 +0530

    feat(git): Conflicts view/resolution
    - Introduces `akinsho/git-conflict.nvim` to power Git
      conflict view and resolution in the default editor view.
    - I must note that the experience is not perfect, nor
      does it match the experience of the Jetbrains/Sublime merge
      3-way resolution. Regardless it is very good to have some
      resolution support until something better can be had.

--- lua/ditsuke/plugins/git.lua
+++ lua/ditsuke/plugins/git.lua
@@ -14,6 +14,21 @@ return {
     },
   },
 
+  -- >  A plugin to visualise and resolve merge conflicts in neovim
+  -- Bindings (default):
+  -- > This plugin offers default buffer local mappings inside conflicted files. This is primarily because applying these mappings only to relevant buffers is impossible through global mappings. A user can however disable these by setting default_mappings = false anyway and create global mappings as shown below. The default mappings are:
+  -- > - co — choose ours
+  -- > - ct — choose theirs
+  -- > - cb — choose both
+  -- > - c0 — choose none
+  -- > - ]x — move to previous conflict
+  -- > - [x — move to next conflict
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    config = true,
+  },
+
   -- Create and switch worktrees with a telescope-driven UI.
   --
   -- STATUS(2023-04-08): I like it, but I've barely ever used it.
