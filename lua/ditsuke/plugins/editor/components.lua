436ec1026fe699672c58369f82daece5cd59c5ea
ditsuke <ditsuke@protonmail.com>
2023-05-11 16:19:17 +0530

    feat(keymaps)!: Improve harpoon keybindings
    - <C-A> for harpooned files view
    - <Tab>/<S-Tab> to cycle through harpoon files.
    - <leader>a to mark files

--- lua/ditsuke/plugins/editor/components.lua
+++ lua/ditsuke/plugins/editor/components.lua
@@ -4,8 +4,10 @@ return {
     "ThePrimeagen/harpoon",
     event = "VeryLazy",
     keys = {
-      { "m", function() require("harpoon.mark").add_file() end, desc = "Harpoon: [m]ark file" },
-      { "`", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon: toggle quick menu" },
+      { "<leader>a", function() require("harpoon.mark").add_file() end, desc = "Harpoon: [m]ark file" },
+      { "<C-A>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon: toggle quick menu" },
+      { "<Tab>", function() require("harpoon.ui").nav_next() end, desc = "Harpoon: Nav next" },
+      { "<S-Tab>", function() require("harpoon.ui").nav_prev() end, desc = "Harpoon: Nav prev" },
     },
   },
   {
