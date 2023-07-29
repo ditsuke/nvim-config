bc2bca5cc3fe7a288ea7b0c0534503fa88df9514
ditsuke <ditsuke@protonmail.com>
2023-07-29 12:54:03 +0530

    feat(editor/motions)!: Disable flit.nvim/leap.nvim
    I'm experimenting with flash.nvim, replacing leap and flit if I like
    it.

--- lua/ditsuke/plugins/editor/motions-and-objects.lua
+++ lua/ditsuke/plugins/editor/motions-and-objects.lua
@@ -1,7 +1,9 @@
 return {
   {
     -- _Better_ f/t motions for `leap.nvim`
+    -- TODO: remove in favour of `flash.nvim` (which is already the default and enabled at the time of writing)
     "ggandor/flit.nvim",
+    enabled = false,
     opts = {
       multiline = false,
     },
