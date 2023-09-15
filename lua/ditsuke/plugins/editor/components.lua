7bd6446ae2af372ceda5544f31b657f4eca68bf0
ditsuke <ditsuke@protonmail.com>
2023-09-15 23:42:09 +0530

    refactor: editor/harpoon to its own file

--- lua/ditsuke/plugins/editor/components.lua
+++ lua/ditsuke/plugins/editor/components.lua
@@ -1,15 +1,4 @@
 return {
-  {
-    -- Jump between files and terminals
-    "ThePrimeagen/harpoon",
-    event = "VeryLazy",
-    keys = {
-      { "<leader>a", function() require("harpoon.mark").add_file() end, desc = "Harpoon: [m]ark file" },
-      { "<C-A>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon: toggle quick menu" },
-      { "<Tab>", function() require("harpoon.ui").nav_next() end, desc = "Harpoon: Nav next" },
-      { "<S-Tab>", function() require("harpoon.ui").nav_prev() end, desc = "Harpoon: Nav prev" },
-    },
-  },
 
   {
     "L3MON4D3/LuaSnip",
