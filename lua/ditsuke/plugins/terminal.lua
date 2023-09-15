debe55bbdd27855e39c1356c28803500bd2a3428
ditsuke <ditsuke@protonmail.com>
2023-09-15 23:40:48 +0530

    feat(components/terminal): Update config

--- lua/ditsuke/plugins/terminal.lua
+++ lua/ditsuke/plugins/terminal.lua
@@ -11,20 +11,37 @@ end
 return {
   {
     "akinsho/toggleterm.nvim",
-    config = true,
-    keys = function() -- either lazy.nvim must stop evaluating this with plugin not loaded or I need to adopt another strategy.
-      return {
-        {
-          "<F60>",
-          toggleterm,
-          mode = { "n", "t", "i" },
-        },
-        {
-          "<C-\\>",
-          toggleterm,
-          mode = { "n", "t", "i" },
-        },
-      }
-    end,
+    opts = {
+      winbar = {
+        enabled = true,
+        name_formatter = function(term) --  term: Terminal
+          return term.name
+        end,
+      },
+      float_opts = {
+        -- The border key is almost the same as 'nvim_open_win'
+        -- see :h nvim_open_win for details on borders however
+        -- the 'curved' border is a custom border type
+        -- not natively supported but implemented in this plugin.
+        border = "curved",
+        -- like `size`, width and height can be a number or function which is passed the current terminal
+        -- width = <value>,
+        -- height = <value>,
+        winblend = 10,
+        zindex = 100,
+      },
+    },
+    keys = {
+      {
+        "<F60>",
+        toggleterm,
+        mode = { "n", "t", "i" },
+      },
+      {
+        "<C-\\>",
+        toggleterm,
+        mode = { "n", "t", "i" },
+      },
+    },
   },
 }
