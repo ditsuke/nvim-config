20cc8f01289768263636dcccc648bfe608cc2cb6
ditsuke <ditsuke@protonmail.com>
2025-01-07 05:29:13 +0530

    chore: Update blink.cmp config
    Getting closer to my workflow..

--- lua/ditsuke/plugins/editor/blink.cmp.lua
+++ lua/ditsuke/plugins/editor/blink.cmp.lua
@@ -1,9 +1,18 @@
 return {
   "saghen/blink.cmp",
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
   opts = {
+    completion = {
+      list = {
+        selection = "auto_insert",
+      },
+    },
     keymap = {
       ["<C-j>"] = { "select_next", "fallback" },
       ["C-k>"] = { "select_prev", "fallback" },
+      ["<Tab>"] = { "select_next" },
     },
   },
 }
