b2de626fafc082a49f56b85e567c0a6f77d635a6
ditsuke <ditsuke@protonmail.com>
2026-01-14 08:13:17 +0530

    feat: Update lsp breadcrumbs in winbar

--- lua/ditsuke/plugins/ui/winbar.lua
+++ lua/ditsuke/plugins/ui/winbar.lua
@@ -15,7 +15,7 @@ return {
   opts = {
     -- The navic config that ships with Lazyvim already attaches
     -- for us, besides `barbecue/issue#35`
-    attach_navic = false,
+    attach_navic = true,
     -- Window number as leading section
     lead_custom_section = function(_, winnr) return string.format("  %d 󱋱 ", vim.api.nvim_win_get_number(winnr)) end,
 
